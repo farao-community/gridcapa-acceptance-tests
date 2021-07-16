8de126940bd765cd21dc60f4bf1b9e80483c2618
pascolithe <theo.pascoli@rte-france.com>
2021-07-16 14:58:55 +0200

    merge conflit

--- cypress/integration/us-0000-acceptance-test-spec.js
+++ cypress/integration/us-0000-acceptance-test-spec.js
@@ -1,16 +1,11 @@
 import * as gc from "../support/function";
+import * as ftp from "../support/ftp-browser.js";
+
+const ftpUser = Cypress.env('GRIDCAPA_FTP_USER');
+const ftpPassword = Cypress.env('GRIDCAPA_FTP_PASSWORD');
 
 describe('CGM automatic import handling', () => {
     it('Triggers one task creation when CGM archive with one CGM arrives', () => {
-        clearAndVisit('/cse/d2cc');
-        authentication();
-        getTimestampView();
-        ftp.runOnFtp(ftpUser, ftpPassword, () => {
-            ftp.copyFileToFtp('us-0000/20210701.zip', '/2021/07/01');
-        });
-        ftp.runOnFtp(ftpUser, ftpPassword, () => {
-            ftp.deleteFileFromFtp('/2021/07/01/20210701.zip');
-        });
         gc.clearAndVisit('/cse/d2cc')
         gc.authentication()
         gc.getTimestampView()
@@ -20,5 +15,12 @@ describe('CGM automatic import handling', () => {
         cy.get('[data-test=input-status]').should('have.text','Absent')
         cy.get('[data-test=input-filename]').should('be.empty')
         cy.get('[data-test=input-latest-modification]').should('be.empty')
+
+        ftp.runOnFtp(ftpUser, ftpPassword, () => {
+            ftp.copyFileToFtp('us-0000/20210701.zip', '/2021/07/01');
+        });
+        ftp.runOnFtp(ftpUser, ftpPassword, () => {
+            ftp.deleteFileFromFtp('/2021/07/01/20210701.zip');
+        });
     });
 })
