607d92d5994cdef3f41f551c1bced0c47d33555c
pascolithe <theo.pascoli@rte-france.com>
2021-07-16 13:21:20 +0200

    Add tests for the overview panel in the table

--- cypress/integration/us-0000-acceptance-test-spec.js
+++ cypress/integration/us-0000-acceptance-test-spec.js
@@ -1,17 +1,15 @@
-import {authentication, clearAndVisit, getTimestampView, setupDateAndTime} from "./function";
+import * as gc from "../support/function";
 
 describe('CGM automatic import handling', () => {
-
-
     it('Triggers one task creation when CGM archive with one CGM arrives', () => {
-        clearAndVisit('/cse/d2cc')
-        authentication()
-        getTimestampView()
-        setupDateAndTime('2021-07-01', '14:30')
+        gc.clearAndVisit('/cse/d2cc')
+        gc.authentication()
+        gc.getTimestampView()
+        gc.setupDateAndTime('2021-07-01', '14:30')
         cy.get('[data-test=timestamp-status]').should('have.text','Not created')
         cy.get('[data-test=input-type]').should("have.text","CGM")
-        cy.get('[data-test=file-status]').should('have.text','Absent')
-        cy.get('[data-test=filename]').should('be.empty')
-        cy.get('[data-test=message]').should('be.empty')
+        cy.get('[data-test=input-status]').should('have.text','Absent')
+        cy.get('[data-test=input-filename]').should('be.empty')
+        cy.get('[data-test=input-latest-modification]').should('be.empty')
     });
 })
--- cypress/support/function.js
+++ cypress/support/function.js
@@ -12,6 +12,6 @@ export function getTimestampView() {
     cy.get('[data-test=timestamp-view]').click()
 }
 export function setupDateAndTime(date, time) {
-    cy.get('input[type=date]').type(date)
-    cy.get('input[type=time]').type(time)
+    cy.get('[data-test=timestamp-date-picker]').type(date)
+    cy.get('[data-test=timestamp-time-picker]').type(time)
 }
