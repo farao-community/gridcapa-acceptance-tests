ab7f651117b8f0bfa2b95496d6555fc2e768cafc
Sébastien Murgey <sebastien.murgey@rte-france.com>
2021-07-22 22:01:46 +0200

    Test other authentication implementation

--- cypress/integration/us-0000-acceptance-test-spec.js
+++ cypress/integration/us-0000-acceptance-test-spec.js
@@ -2,14 +2,15 @@ import * as gc from "../support/function";
 
 describe('CGM automatic import handling', () => {
     it('Triggers one task creation when CGM archive with one CGM arrives', () => {
-        gc.clearAndVisit('/cse/d2cc')
-        gc.authentication()
-        gc.getTimestampView()
-        gc.setupDateAndTime('2021-07-01', '14:30')
-        cy.get('[data-test=timestamp-status]').should('have.text','Not created')
-        cy.get('[data-test=input-type]').should("have.text","CGM")
-        cy.get('[data-test=input-status]').should('have.text','Absent')
-        cy.get('[data-test=input-filename]').should('be.empty')
-        cy.get('[data-test=input-latest-modification]').should('be.empty')
+        gc.runOnGridCapa('/cse/d2cc',() => {
+            gc.authentication()
+            gc.getTimestampView()
+            gc.setupDateAndTime('2021-07-01', '14:30')
+            cy.get('[data-test=timestamp-status]').should('have.text','Not created')
+            cy.get('[data-test=input-type]').should("have.text","CGM")
+            cy.get('[data-test=input-status]').should('have.text','Absent')
+            cy.get('[data-test=input-filename]').should('be.empty')
+            cy.get('[data-test=input-latest-modification]').should('be.empty')
+        })
     });
 })
--- cypress/support/function.js
+++ cypress/support/function.js
@@ -1,14 +1,11 @@
-export function clearAndVisit(link) {
-    cy.visit(link, {
-        onBeforeLoad(win) {
-            win.sessionStorage.clear()
-        }
-    })
+export function runOnGridCapa(baseUrl, lambda) {
+    connectToGridCapa(baseUrl);
+    lambda();
+    disconnectFromGridCapa();
 }
 export function authentication() {
-    cy.wait(100)
     cy.get('button').click()
-    cy.wait(100)
+    cy.wait(1000)
 }
 export function getTimestampView() {
     cy.get('[data-test=timestamp-view]').click()
@@ -16,4 +13,19 @@ export function getTimestampView() {
 export function setupDateAndTime(date, time) {
     cy.get('[data-test=timestamp-date-picker]').type(date)
     cy.get('[data-test=timestamp-time-picker]').type(time)
+}
+function connectToGridCapa(baseUrl) {
+    clearAndVisit(baseUrl)
+}
+function clearAndVisit(link) {
+    cy.visit(link, {
+        onBeforeLoad(win) {
+            win.sessionStorage.clear()
+        }
+    })
+    cy.wait(1000)
+}
+function disconnectFromGridCapa() {
+    cy.get('button[aria-controls=settings-menu]').click()
+    cy.get('#settings-menu :nth-child(7)').click()
 }
