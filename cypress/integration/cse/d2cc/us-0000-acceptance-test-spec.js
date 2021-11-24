cf91c5d5e7c33aa60d7f76320ea724958895035f
Joris Mancini <joris.mancini@rte-france.com>
2021-11-24 09:55:18 +0100

    Enable direct upload/delete on FTP and change CI for direct FTP

--- cypress/integration/cse/d2cc/us-0000-acceptance-test-spec.js
+++ cypress/integration/cse/d2cc/us-0000-acceptance-test-spec.js
@@ -11,6 +11,7 @@ import * as minio from "../../../support/minio.js";
 
 const fbUser = Cypress.env('GRIDCAPA_FB_USER');
 const fbPassword = Cypress.env('GRIDCAPA_FB_PASSWORD')
+const fbRootDirectory = '/ftp/cse/d2cc/'
 const minioUser = Cypress.env('GRIDCAPA_MINIO_USER');
 const minioPassword = Cypress.env('GRIDCAPA_MINIO_PASSWORD')
 
@@ -19,6 +20,26 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     // failing the test
     return false
 })
+
+export function uploadOnFtp(file, path) {
+    if (ftpHost) {
+        ftp.uploadOnFtpByCommand(ftpHost, ftpUser, ftpPassword, file, ftpRootDirectory + path)
+    } else {
+        ftp.runOnFtp(fbUser, fbPassword, () => {
+            ftp.copyZipToFtp(file, fbRootDirectory + path);
+        });
+    }
+}
+
+export function deleteOnFtp(file) {
+    if (ftpHost) {
+        ftp.deleteOnFtpByCommand(ftpHost, ftpUser, ftpPassword, ftpRootDirectory + file)
+    } else {
+        ftp.runOnFtp(fbUser, fbPassword, () => {
+            ftp.deleteFileFromFtp(fbRootDirectory + file);
+        });
+    }
+}
 
 describe('CGM automatic import handling', () => {
     it('Triggers one task creation when CGM archive with one CGM arrives', () => {
@@ -42,14 +63,10 @@ describe('CGM automatic import handling', () => {
         gc.clearAndVisit('/cse/d2cc')
         gc.authentication()
         cgm.checkUnloadedCgmsOnBD('2021-07-02')
-        ftp.runOnFtp(fbUser, fbPassword, () => {
-            ftp.copyZipToFtp('us-0000/20210702.zip', '/ftp/cse/d2cc/cgms');
-        });
+        uploadOnFtp('us-0000/20210702.zip', 'cgms')
         cy.visit('/cse/d2cc')
         cgm.checkLoadedCgmsOnBD('2021-07-02', '20210702_{0}30_2D5_UX0.uct')
-        ftp.runOnFtp(fbUser, fbPassword, () => {
-            ftp.deleteFileFromFtp('/ftp/cse/d2cc/cgms/20210702.zip');
-        });
+        deleteOnFtp('cgms/20210702.zip')
         minio.runOnMinio(minioUser, minioPassword, () => {
             minio.deleteHourlyFilesFromMinio('/gridcapa/CSE/D2CC/CGMs/', '20210702_{0}30_2D5_UX0.uct')
         })
@@ -59,29 +76,21 @@ describe('CGM automatic import handling', () => {
         gc.clearAndVisit('/cse/d2cc')
         gc.authentication()
         cgm.checkUnloadedCgm('2021-07-03', '00:30')
-        ftp.runOnFtp(fbUser, fbPassword, () => {
-            ftp.copyZipToFtp('us-0000/20210703.zip', '/ftp/cse/d2cc/cgms');
-        });
+        uploadOnFtp('us-0000/20210703.zip', 'cgms')
         cy.visit('/cse/d2cc')
         cy.wait(10000) // To make sure nothing is created even after a couple of seconds
         cgm.checkUnloadedCgm('2021-07-03', '00:30')
-        ftp.runOnFtp(fbUser, fbPassword, () => {
-            ftp.deleteFileFromFtp('/ftp/cse/d2cc/cgms/20210703.zip');
-        });
+        deleteOnFtp('cgms/20210703.zip')
     });
 
     it('Triggers multiple task creation when CGM archive with multiple CGM and subdirectories arrives', () => {
         gc.clearAndVisit('/cse/d2cc')
         gc.authentication()
         cgm.checkUnloadedCgmsOnBD('2021-07-02')
-        ftp.runOnFtp(fbUser, fbPassword, () => {
-            ftp.copyZipToFtp('us-0000/20210702_sub_dir.zip', '/ftp/cse/d2cc/cgms');
-        });
+        uploadOnFtp('us-0000/20210702_sub_dir.zip', 'cgms')
         cy.visit('/cse/d2cc')
         cgm.checkLoadedCgmsOnBD('2021-07-02', '20210702_{0}30_2D5_UX0.uct')
-        ftp.runOnFtp(fbUser, fbPassword, () => {
-            ftp.deleteFileFromFtp('/ftp/cse/d2cc/cgms/20210702_sub_dir.zip');
-        });
+        deleteOnFtp('cgms/20210702_sub_dir.zip')
         minio.runOnMinio(minioUser, minioPassword, () => {
             minio.deleteHourlyFilesFromMinio('/gridcapa/CSE/D2CC/CGMs/', '20210702_{0}30_2D5_UX0.uct')
         })
@@ -91,14 +100,10 @@ describe('CGM automatic import handling', () => {
         gc.clearAndVisit('/cse/d2cc')
         gc.authentication()
         cgm.checkUnloadedCgm('2021-07-02', '23:30')
-        ftp.runOnFtp(fbUser, fbPassword, () => {
-            ftp.copyFileToFtp('us-0000/20210702_2330_2D5_UX0.uct', '/ftp/cse/d2cc/cgms');
-        });
+        uploadOnFtp('us-0000/20210702_2330_2D5_UX0.uct', 'cgms')
         cy.visit('/cse/d2cc')
         cgm.checkLoadedCgm('2021-07-02', '23:30', '20210702_2330_2D5_UX0.uct')
-        ftp.runOnFtp(fbUser, fbPassword, () => {
-            ftp.deleteFileFromFtp('/ftp/cse/d2cc/cgms/20210702_2330_2D5_UX0.uct');
-        });
+        deleteOnFtp('cgms/20210702_2330_2D5_UX0.uct')
         minio.runOnMinio(minioUser, minioPassword, () => {
             minio.deleteFileFromMinio('/gridcapa/CSE/D2CC/CGMs/', '20210702_2330_2D5_UX0.uct')
         })
