6191a755e85cf33ca8a09d98fb4d34ec7f1eaa91
Joris Mancini <joris.mancini@rte-france.com>
2021-11-24 10:40:55 +0100

    Commit shelved changes

--- cypress/integration/cse/d2cc/us-0000-acceptance-test-spec.js
+++ cypress/integration/cse/d2cc/us-0000-acceptance-test-spec.js
@@ -27,22 +27,22 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 })
 
 export function uploadOnFtp(file, path) {
-    if (ftpHost === '') {
+    if (ftpHost) {
+        ftp.uploadOnFtpByCommand(ftpHost, ftpUser, ftpPassword, file, ftpRootDirectory + path)
+    } else {
         ftp.runOnFtp(fbUser, fbPassword, () => {
             ftp.copyZipToFtp(file, fbRootDirectory + path);
         });
-    } else {
-        ftp.uploadOnFtpByCommand(ftpHost, ftpUser, ftpPassword, file, ftpRootDirectory + path)
     }
 }
 
 export function deleteOnFtp(file) {
-    if (ftpHost === '') {
+    if (ftpHost) {
+        ftp.deleteOnFtpByCommand(ftpHost, ftpUser, ftpPassword, ftpRootDirectory + file)
+    } else {
         ftp.runOnFtp(fbUser, fbPassword, () => {
             ftp.deleteFileFromFtp(fbRootDirectory + file);
         });
-    } else {
-        ftp.deleteOnFtpByCommand(ftpHost, ftpUser, ftpPassword, ftpRootDirectory + file)
     }
 }
 
@@ -51,14 +51,10 @@ describe('CGM automatic import handling', () => {
         gc.clearAndVisit('/cse/d2cc')
         gc.authentication()
         cgm.checkUnloadedCgm('2021-07-01', '14:30')
-        ftp.runOnFtp(fbUser, fbPassword, () => {
-            ftp.copyZipToFtp('us-0000/20210701.zip', '/ftp/cse/d2cc/cgms');
-        });
+        uploadOnFtp('us-0000/20210701.zip', 'cgms')
         cy.visit('/cse/d2cc')
         cgm.checkLoadedCgm('2021-07-01', '14:30', '20210701_1430_2D4_UX0.uct')
-        ftp.runOnFtp(fbUser, fbPassword, () => {
-            ftp.deleteFileFromFtp('/ftp/cse/d2cc/cgms/20210701.zip', 'base64');
-        });
+        deleteOnFtp('cgms/20210701.zip')
         minio.runOnMinio(minioUser, minioPassword, () => {
             minio.deleteFileFromMinio('/gridcapa/CSE/D2CC/CGMs/', '20210701_1430_2D4_UX0.uct')
         })
