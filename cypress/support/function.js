7bfcef1f179f76a18a08ccd329a0f275ef7ce8c5
benrejebmoh <48012306+benrejebmoh@users.noreply.github.com>
2021-12-17 15:32:41 +0100

    cypress test for gcr 84 - inputs events display (#23)
    * cypress test for gcr 84 - inputs events display
    
    * minor refactoring related to feature review

--- cypress/support/function.js
+++ cypress/support/function.js
@@ -4,6 +4,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+import crypto from "crypto";
+
 const DEFAULT_FTP_UPLOAD_TIMEOUT_IN_S = 30;
 const S_TO_MS_FACTOR = 1000;
 const DEFAULT_FTP_UPLOAD_TIMEOUT_IN_MS = DEFAULT_FTP_UPLOAD_TIMEOUT_IN_S * S_TO_MS_FACTOR;
@@ -15,14 +17,21 @@ export function clearAndVisit(link) {
         }
     })
 }
+
 export function authentication() {
     cy.get('button').click()
 }
+
 export function getTimestampView() {
     cy.get('[data-test=timestamp-view]').click()
 }
+
 export function setupDate(date) {
     cy.get('[data-test=timestamp-date-picker]').type(date)
+}
+
+export function clickOnEventsTab() {
+    cy.get('[data-test=events]').click()
 }
 
 export function selectTimestampViewForDate(date) {
@@ -35,7 +44,7 @@ export function setupTime(time) {
 }
 
 export function timestampStatusShouldBe(timestampStatus) {
-    cy.get('[data-test=timestamp-status]', {timeout: DEFAULT_FTP_UPLOAD_TIMEOUT_IN_MS}).should('have.text',timestampStatus)
+    cy.get('[data-test=timestamp-status]', {timeout: DEFAULT_FTP_UPLOAD_TIMEOUT_IN_MS}).should('have.text', timestampStatus)
 }
 
 export function runButtonStatusShouldBeDisabled() {
@@ -52,8 +61,8 @@ export function clickRunButton() {
 
 export function inputDataShouldBe(expectedType, expectedStatus, expectedFilename, expectedLatestModification) {
     const timeoutProps = {timeout: DEFAULT_FTP_UPLOAD_TIMEOUT_IN_MS}
-    cy.get('[data-test=' + expectedType + '-input-type]', timeoutProps).should('have.text',expectedType)
-    cy.get('[data-test=' + expectedType + '-input-status]', timeoutProps).should('have.text',expectedStatus)
+    cy.get('[data-test=' + expectedType + '-input-type]', timeoutProps).should('have.text', expectedType)
+    cy.get('[data-test=' + expectedType + '-input-status]', timeoutProps).should('have.text', expectedStatus)
     if (expectedFilename) {
         cy.get('[data-test=' + expectedType + '-input-filename]', timeoutProps).should('have.text', expectedFilename)
     } else {
@@ -65,3 +74,16 @@ export function inputDataShouldBe(expectedType, expectedStatus, expectedFilename
         cy.get('[data-test=' + expectedType + '-input-latest-modification]', timeoutProps).should('be.empty')
     }
 }
+
+export function sha256(param) {
+    return crypto.createHash('sha256').update(param, 'utf8').digest('hex'); // UTF8 text hash
+}
+
+export function checkFileEventDisplayed(expectedLevel, expectedMessage) {
+    const timeoutProp = {timeout: DEFAULT_FTP_UPLOAD_TIMEOUT_IN_MS}
+    cy.get('[data-test=' + sha256(expectedMessage) + '-process-event-level]', timeoutProp).should('have.text', expectedLevel)
+    cy.get('[data-test=' + sha256(expectedMessage) + '-process-event-message]', timeoutProp).should('have.text', expectedMessage)
+    cy.get('[data-test=' + sha256(expectedMessage) + '-process-event-timestamp]').should('not.be.empty')
+}
+
+
