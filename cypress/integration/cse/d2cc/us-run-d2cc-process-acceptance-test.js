bc929c6a6ae8deb517e418e12041139687653257
AMIRA KAHYA <amira.kahya@rte-france.com>
2022-04-28 10:11:41 +0200

    run a full d2cc process

---
+++ cypress/integration/cse/d2cc/us-run-d2cc-process-acceptance-test.js
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+import * as gc from "../../../support/function";
+import * as minio from "../../../support/minio";
+import * as ftp from "../../../support/ftp-browser.js";
+import {
+    clickRunButton,
+    runButtonStatusShouldBeEnabled,
+    selectTimestampViewForDate,
+    timestampStatusShouldBe
+} from "../../../support/function";
+
+const minioUser = Cypress.env('GRIDCAPA_MINIO_USER');
+const minioPassword = Cypress.env('GRIDCAPA_MINIO_PASSWORD')
+const cgm = 'cgms';
+const glsk = 'glsks';
+const crac = 'cracs';
+const ntc = "ntc";
+const ntcred = "ntcreds"
+const TIMEOUT = 60000
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+})
+
+describe('Run d2cc process', () => {
+
+    it('Run a full d2cc process ', () => {
+        ftp.uploadOnFtp('CSE_D2CC', 'grc-inputs-files-d2cc-process/20210901_2230_test_network.uct', cgm)
+        ftp.uploadOnFtp('CSE_D2CC', 'grc-inputs-files-d2cc-process/20210901_2230_213_GSK_CO_CSE1.xml', glsk)
+        ftp.uploadOnFtp('CSE_D2CC', 'grc-inputs-files-d2cc-process/20210901_2230_213_CRAC_CO_CSE1.xml', crac)
+        ftp.uploadOnFtp('CSE_D2CC', 'grc-inputs-files-d2cc-process/2021_test_NTC_annual.xml', ntc)
+        ftp.uploadOnFtp('CSE_D2CC', 'us-import-daily-files/20210901_2D3_NTC_reductions_test.xml', ntcred)
+        gc.clearAndVisit('/cse/d2cc')
+        gc.authentication()
+        selectTimestampViewForDate("2021-09-01")
+        gc.setupTime("22:30")
+        timestampStatusShouldBe('READY', TIMEOUT)
+        runButtonStatusShouldBeEnabled()
+        clickRunButton()
+        timestampStatusShouldBe('RUNNING', TIMEOUT)
+        timestampStatusShouldBe('SUCCESS', TIMEOUT)
+    })
+    it("Delete files from minio and FTP", () => {
+        minio.runOnMinio(minioUser, minioPassword, () => {
+            minio.deleteFilesFromMinio([
+                '/gridcapa/CSE/D2CC/CGMs/20210901_2230_test_network.uct',
+                '/gridcapa/CSE/D2CC/GLSKs/20210901_2230_213_GSK_CO_CSE1.xml',
+                '/gridcapa/CSE/D2CC/CRACs/20210901_2230_213_CRAC_CO_CSE1.xml',
+                '/gridcapa/CSE/D2CC/NTC/2021_test_NTC_annual.xml',
+                '/gridcapa/CSE/D2CC/NTCREDs/20210901_2D3_NTC_reductions_test.xml',
+                '/gridcapa/CSE/D2CC/2021/09/01/22_30/ARTIFACTS/raoParameters.json',
+                '/gridcapa/CSE/D2CC/2021/09/01/22_30/ARTIFACTS/crac.json',
+                '/gridcapa/CSE/D2CC/2021/09/01/22_30/ARTIFACTS/network_pre_processed.xiidm',
+                '/gridcapa/CSE/D2CC/2021/09/01/22_30/OUTPUTS/20210901_2230_2D3_CO_CSE1.uct',
+                '/gridcapa/CSE/D2CC/2021/09/01/22_30/OUTPUTS/TTC_Calculation_20210901_2230_2D0_CO_CSE1.xml'
+            ]);
+        });
+        ftp.deleteOnFtp('CSE_D2CC', 'cgms/20210901_2230_test_network.uct')
+        ftp.deleteOnFtp('CSE_D2CC', 'glsks/20210901_2230_213_GSK_CO_CSE1.xml')
+        ftp.deleteOnFtp('CSE_D2CC', 'cracs/20210901_2230_213_CRAC_CO_CSE1.xm')
+        ftp.deleteOnFtp('CSE_D2CC', 'ntc/2021_test_NTC_annual.xml')
+        ftp.deleteOnFtp('CSE_D2CC', 'ntcreds/20210901_2D3_NTC_reductions_test.xml')
+    })
+
+})
