25b0d75731836a9ccf8744fab9184eff560009b7
benrejebmoh <mohamed.ben-rejeb@rte-france.com>
2022-06-17 18:45:35 +0200

    adapt integration tests after making target ch files user inputs

--- cypress/integration/run-processes/run-import-d2cc-process-acceptance-test.js
+++ cypress/integration/run-processes/run-import-d2cc-process-acceptance-test.js
@@ -20,6 +20,7 @@ const cgm = 'cgms';
 const glsk = 'glsks';
 const crac = 'cracs';
 const ntc = 'ntc';
+const targetchs = 'targetchs'
 const ntcred = 'ntcreds'
 const TIMEOUT_UPLOAD = 120000
 const TIMEOUT = 60000
@@ -36,6 +37,7 @@ describe('Check CSE D2CC import corner runs correctly', () => {
         ftp.uploadOnFtp('CSE_D2CC', 'grc-inputs-files-d2cc-process/20210901_2230_213_GSK_CO_CSE1.xml', glsk)
         ftp.uploadOnFtp('CSE_D2CC', 'grc-inputs-files-d2cc-process/20210901_2230_213_CRAC_CO_CSE1.xml', crac)
         ftp.uploadOnFtp('CSE_D2CC', 'grc-inputs-files-d2cc-process/2021_test_NTC_annual.xml', ntc)
+        ftp.uploadOnFtp('CSE_D2CC', 'grc-inputs-files-d2cc-process/2021_target_ch_annual.xml', targetchs)
         ftp.uploadOnFtp('CSE_D2CC', 'us-import-daily-files/20210901_2D3_NTC_reductions_test.xml', ntcred)
         gc.clearAndVisit('/cse/import/d2cc')
         gc.authentication()
@@ -54,6 +56,7 @@ describe('Check CSE D2CC import corner runs correctly', () => {
         ftp.deleteOnFtp('CSE_D2CC', 'glsks/20210901_2230_213_GSK_CO_CSE1.xml')
         ftp.deleteOnFtp('CSE_D2CC', 'cracs/20210901_2230_213_CRAC_CO_CSE1.xm')
         ftp.deleteOnFtp('CSE_D2CC', 'ntc/2021_test_NTC_annual.xml')
+        ftp.deleteOnFtp('CSE_D2CC', 'targetchs/2021_target_ch_annual.xml')
         ftp.deleteOnFtp('CSE_D2CC', 'ntcreds/20210901_2D3_NTC_reductions_test.xml')
     })
 })
