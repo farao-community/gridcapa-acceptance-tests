06a2a2ddffe18e6684d138602df9f018fe8d0a4c
TheMaskedTurtle <53527338+TheMaskedTurtle@users.noreply.github.com>
2022-07-19 11:57:15 +0200

    Add test for fixed timestamp between tabs (#40)
    * Add new test for common timestamp management and delete minIO files by folder
    
    * Adapt test to refactoring

--- cypress/support/gridcapa-util.js
+++ cypress/support/gridcapa-util.js
@@ -63,6 +63,14 @@ export function runComputation() {
     cy.get('[data-test=run-button]').click();
 }
 
+export function dateShouldBe(date) {
+    cy.get('[data-test=timestamp-date-picker]').should('have.value', date)
+}
+
+export function timeShouldBe(time) {
+    cy.get('[data-test=timestamp-time-picker]').should('have.value', time)
+}
+
 export function runComputationForTimestamp(timestamp) {
     cy.get('[data-test=run-button-'+ timestamp +']').click();
 }
